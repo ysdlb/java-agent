fb20d094627e717e2cd0f3cd4f7372efa294f2e2
gqhan <libertyhan@outlook.com>
2022-04-02 10:12:30 +0800

    Update DemoAgent.java

--- java-probe-agent/src/main/java/learn/probe/agent/DemoAgent.java
+++ java-probe-agent/src/main/java/learn/probe/agent/DemoAgent.java
@@ -2,6 +2,7 @@ package learn.probe.agent;
 
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
+import java.security.ProtectionDomain;
 
 public class DemoAgent {
 
@@ -22,7 +23,7 @@ public class DemoAgent {
      * 如果 目标类 在 attach 之间就完成了类加载, 那么我们调用 retransform 触发重新加载一次没问题
      * 如果 目标类 在 attach 之前从未使用（没有加载
      * 此时 Class.forName 会触发一次类加载, retransform 也会触发一次, 这里会调用两次
-     * {@link DemoTransformer#transform()}
+     * {@link DemoTransformer#transform}
      *
      * 仅判断一个类是否被加载过但不触发加载动作但骚操作详见：
      * https://stackoverflow.com/questions/482633/in-java-is-it-possible-to-know-whether-a-class-has-already-been-loaded
