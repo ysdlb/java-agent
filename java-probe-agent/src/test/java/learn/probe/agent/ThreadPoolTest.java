e50814072044a98a0e2c3838941049001c2ab3ae
gqhan <libertyhan@outlook.com>
2022-07-19 08:50:24 +0800

    添加单元测试

---
+++ java-probe-agent/src/test/java/learn/probe/agent/ThreadPoolTest.java
@@ -0,0 +1,39 @@
+package learn.probe.agent;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(ThreadPoolTest.class);
+
+    @Test
+    void threadPoolExecute() {
+        int size = 2;
+        ExecutorService executorPools = Executors.newFixedThreadPool(size);
+        ExecutorService childExecutorPools = Executors.newFixedThreadPool(size);
+        ExecutorService grandSonExecutorPools = Executors.newFixedThreadPool(size);
+        for (int i = 0; i < size*2; i++) {
+            String taskName = "taskName" + "-" + i;
+            executorPools.submit(() -> {
+                logger.info("{} submit", taskName);
+                childExecutorPools.submit(() -> {
+                    logger.info("{} child submit", taskName);
+                    grandSonExecutorPools.submit(() -> {
+                        logger.info("{} grandSon submit", taskName);
+                    });
+                });
+            });
+        }
+    }
+
+    @Test
+    void completableFutureTest() {
+        CompletableFuture.supplyAsync()
+    }
+}
